2790376869074ccd00cdfa3a911b19d25d7ab153
Praveen Kumar <praveen.kumar@thinkitive.com>
2024-09-20 19:46:35 +0530

    updated the assignment with some functionality

--- assignment/index.js
+++ assignment/index.js
@@ -8,7 +8,6 @@ let result = `<table>
 </thead><tbody>`;
 let end = `</tbody></table>`;
 let arr = [];
-
 function task() {
     let firstname = document.getElementById("fname").value; 
     let lastname = document.getElementById("lname").value;
@@ -24,7 +23,6 @@ function task() {
             </tr>`; 
         }
         document.getElementById("result").innerHTML = result + row + end;
-
         document.getElementById("fname").value = "";
         document.getElementById("lname").value = "";
         document.getElementById("email").value = "";
@@ -33,4 +31,52 @@ function task() {
         alert("Please fill in all fields.");
     }
 }
-document.getElementById("click").addEventListener('click', task);
+document.getElementById("click").addEventListener('click', (event)=>{
+     event.preventDefault();
+     let firstname = document.getElementById("fname").value; 
+    //  let lastname = document.getElementById("lname").value;
+     let email = document.getElementById("email").value;
+    let valid = true;
+    if (firstname === '') {  
+        document.getElementById('firstNameError').textContent = 'First name is required';
+        valid = false;
+    } if (email === '') {
+        document.getElementById('emailError').textContent = 'Email is required';
+        valid = false;
+      }
+      else{
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailPattern.test(email)) {
+        document.getElementById('emailError').textContent = 'Please enter a valid email';
+        valid = false;
+      } }   
+      if (valid) {
+        document.getElementById('emailError').textContent="";
+        console.log("Im Here");
+        // event.preventDefault();
+        task();
+      } 
+    })
+
+
+    
+
+
+
+
+
+
+// // {
+// //     let data=10;
+// //     data=20;
+// //     console.log(data);
+// // }
+// //  data=30;
+// // console.log(data);
+
+
+// let arr = [1, 2, 3, 4];
+// arr.splice(1, 2, 'a', 'b');
+// // arr.splice(1,1);
+// console.log(arr)
+
